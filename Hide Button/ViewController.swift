14dbe0502da18320133154b8a154fb1dad69012a
Parth Patel <parthpatel@Harshs-MacBook-Pro.local>
2016-07-13 11:07:25 -0400

    added hide-button app files

--- Hide Button/ViewController.swift
+++ Hide Button/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var hideRedButton: UIButton!
+    
+    @IBOutlet weak var showBlueBomb: UIButton!
+    @IBOutlet weak var showRedBomb: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +29,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func hideBlue(sender: AnyObject){
+    
+            blueBomb.hidden = true
+            showBlueBomb.hidden = false
+            hideBlueButton.hidden = true
+    
+    }
+    
+    @IBAction func hideRed(sender: AnyObject){
+        
+        redBomb.hidden = true
+        showRedBomb.hidden = false
+        hideRedButton.hidden = true
+    
+    }
+    
+    @IBAction func showBlue(sender: AnyObject){
+        
+        blueBomb.hidden = false
+        showBlueBomb.hidden = true
+        hideBlueButton.hidden = false
+        
+    }
+    
+    @IBAction func showRed(sender: AnyObject){
+        
+        redBomb.hidden = false
+        showRedBomb.hidden = true
+        hideRedButton.hidden = false
+    }
 }
 
